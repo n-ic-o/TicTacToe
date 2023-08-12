97ca07afebdbe95aab1cbc82d8aeb532de5bf8be
n-ic-o <01lemming-kodonen@icloud.com>
2023-08-13 01:46:41 +0200

    added stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,5 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+
+let xwins = ref(0)
+let owins = ref(0)
+
+const addWin = (winner) => {
+  if (winner === 'x') {
+    xwins.value++
+  } else if (winner === 'o') {
+    owins.value++
+  }
+}
+</script>
+
 <template>
-  <div class="container">
-    <p>Stats</p>
+  <div class="stats">
+    <p class="winstats">{{ xwins }} : {{ owins }}</p>
   </div>
 </template>
+
+<style scoped>
+.stats {
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.winstats {
+  font-size: 12rem;
+  color: var(--color-text);
+}
+</style>
