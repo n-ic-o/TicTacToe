a00078d9bfc3322ff5fdd094fa409a8606353f5c
n-ic-o <01lemming-kodonen@icloud.com>
2023-08-13 00:46:48 +0200

    field clicke events with player change

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,12 +1,28 @@
 <script setup>
+import { ref } from 'vue'
+
 const props = defineProps({
-  id: Number
+  id: Number,
+  currentplayer: Function
 })
+
+const emit = defineEmits(['fieldclicked'])
+
+let fieldvalue = ref('')
+
+const setField = () => {
+  if (fieldvalue.value === '') {
+    fieldvalue.value = props.currentplayer()
+    console.log(props.currentplayer())
+    emit('fieldclicked')
+  }
+}
 </script>
 
 <template>
-  <div class="field" :class="{'border-right': id % 3 !== 0, 'border-left': id % 3 !== 1, 'border-bottom': id < 7, 'border-top': id > 3}">
-    <p>X</p>
+  <div class="field" :class="{'border-right': id % 3 !== 0, 'border-bottom': id < 7}" @click="setField">
+    <i class="fa-solid fa-circle xl" v-if="fieldvalue === 'o'"></i>
+    <i class="fa-solid fa-times xxl" v-if="fieldvalue === 'x'"></i>
   </div>
 </template>
 
@@ -18,18 +34,18 @@ const props = defineProps({
 }
 
 .border-right {
-  border-right: 1px solid black;
-}
-
-.border-left {
-  border-left: 1px solid black;
+  border-right: 1px solid var(--color-text);
 }
 
 .border-bottom {
-  border-bottom: 1px solid black;
+  border-bottom: 1px solid var(--color-text);
 }
 
-.border-top {
-  border-top: 1px solid black;
+.xl {
+  font-size: 7rem;
+}
+
+.xxl {
+  font-size: 10rem;
 }
 </style>
--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -1,10 +1,21 @@
 <script setup>
 import Field from './Field.vue'
+import { ref } from 'vue'
+
+let currentplayer = ref('x')
+
+const handleClick = () => {
+  currentplayer.value = currentplayer.value === 'x' ? 'o' : 'x'
+}
+
+const getcurrentplayer = () => {
+  return currentplayer.value
+}
 </script>
 
 <template>
   <div class="gamefield">
-    <Field v-for="i in 9" :key="i" :id = "i"/>
+    <Field v-for="i in 9" :key="i" :id = "i" @fieldclicked="handleClick" :currentplayer="getcurrentplayer" />
   </div>
 </template>
 
@@ -12,8 +23,11 @@ import Field from './Field.vue'
 .gamefield {
   width: 100%;
   height: 100%;
+  padding: 1rem;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(3, 1fr);
+  border-right: 4px solid var(--color-text);
+  /*box-shadow: 0 0 5px 5px #48abe0;*/
 }
 </style>
